82a8cf6a70306d6af940a527fa5817c7a750a99b
Ersindacalista <153999229+Ersindacalista@users.noreply.github.com>
2024-06-24 18:55:59 +0200

    Add files via upload

---
+++ lua/keymaps.lua
@@ -0,0 +1,37 @@
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+---------------
+--  Neotree  --
+---------------
+
+vim.keymap.set('n', '<C-e>', ':Neotree filesystem reveal left<CR>', opts)
+
+---------------
+---Telescope---
+---------------
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-s>', builtin.find_files, {})
+
